18704609c4eb7f48b979c1bb8529513d5b5bfaad
LordiEnd <tavdaba@gmail.com>
2025-10-26 23:04:48 +1000

    Добавьте файлы проекта.

---
+++ laba_0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloKlenin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Klenin!");
+        }
+    }
+}
